c4621830fffbbaa892103d8e2648b6e3cf82feb0
parsajiravand <parsajiravand@gmail.com>
2023-03-26 01:40:15 +0330

    setup tabs component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
+import { TabContainerComponent } from './tab-container/tab-container.component';
+import { TabComponent } from './tab/tab.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    ModalComponent
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent
   ],
   providers:[
     ModalService
---
+++ src/app/shared/tab-container/tab-container.component.ts
@@ -0,0 +1,30 @@
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tab-container',
+  templateUrl: './tab-container.component.html',
+  styleUrls: ['./tab-container.component.scss'],
+})
+export class TabContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
+
+  constructor() {}
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => (tab.active = false));
+    tab.active = true;
+  }
+}
