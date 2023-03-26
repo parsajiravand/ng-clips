5554fee4b96a7e034ba9c5130842f4f952524ca7
parsajiravand <parsajiravand@gmail.com>
2023-03-27 00:04:46 +0330

    setup register component

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(140),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    ),
+  ]);
+  confirmPassword = new FormControl('', [
+    Validators.required,
+    Validators.pattern(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    ),
+  ]);
+  phoneNumber = new FormControl('');
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
+  });
+}
