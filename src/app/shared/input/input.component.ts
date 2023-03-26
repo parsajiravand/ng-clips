b2a1ae477ffded372d4e72085401fa4e98df8459
parsajiravand <parsajiravand@gmail.com>
2023-03-27 00:05:32 +0330

    setup input reusable component

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+})
+export class InputComponent {
+  @Input() label: string = 'Test';
+  @Input() type?: string = 'text';
+  @Input() control: FormControl = new FormControl({});
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,25 +4,22 @@ import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
 import { TabContainerComponent } from './tab-container/tab-container.component';
 import { TabComponent } from './tab/tab.component';
-
-
-
+import { InputComponent } from './input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ModalComponent,
     TabContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
   ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
     ModalComponent,
     TabContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
   ],
-  providers:[
-    ModalService
-  ]
+  providers: [ModalService],
 })
-export class SharedModule { }
+export class SharedModule {}
