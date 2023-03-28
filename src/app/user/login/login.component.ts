bdb748d086e79b6ae5adb1b3ac65d8d986561e68
parsajiravand <parsajiravand@gmail.com>
2023-03-28 17:11:22 +0330

    setup login form component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  user: { email: string; password: string } = { email: '', password: '' };
 
+  login() {
+    console.log(this.user);
+  }
 }
