dd697643429ac064115e433ef674edf944698032
parsajiravand <parsajiravand@gmail.com>
2023-03-29 17:40:52 +0330

    firebase.initializeApp in main  setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import {environment} from './environments/environment';
+
+firebase.initializeApp(environment.firebase);
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let appInit = false;
+firebase.auth().onAuthStateChanged(()=>{
+  if(!appInit){
+    platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+    
+  }
+  appInit = true;
+})
+
+
+
